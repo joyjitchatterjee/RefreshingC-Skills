673d88c6e915858acb41e49e2b5af5a14f1b6cba
Joyjit Chatterjee <joyjitece@gmail.com>
2020-05-10 10:14:28 +0530

    Create advancedhello-world.cpp

---
+++ advancedhello-world.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+
+{
+
+std::string userName;
+std::string foodName;
+std::cout<<"Enter your name please"<<std::endl;
+std::cin>>userName;
+std::cout<<"Key in your favorite food name!"<<std::endl;
+std::cin>>foodName;
+
+std::cout<<"Hi"<<" "<<userName<<" "<<"I will bring you some"<<" "<<foodName<<" "<<"right away!"<<std::endl;
+
+}
+
